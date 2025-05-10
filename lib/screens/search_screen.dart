8e7e3fff218e59eacb4291acc292ffc200def13f
huanphan93 <tienhuanphan@gmail.com>
2025-05-10 22:39:27 +0700

    cập nhật mic cho tìm kiếm

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:btl_flutter_nhom6/widgets/recipe_card.dart';
 import 'package:btl_flutter_nhom6/screens/recipe_detail_screen.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -10,7 +11,45 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   String _searchText = ""; // Biến lưu trữ từ khóa tìm kiếm
+  //khoi tao speech to text
+  late TextEditingController _controller;
+  stt.SpeechToText speech=stt.SpeechToText();
+  bool isListening=false;
 
+  @override
+  void initState(){
+    super.initState();
+    speech = stt.SpeechToText();
+    _controller = TextEditingController();
+  }
+  void onlisten() async{
+    if(!isListening){
+      bool available= await speech.initialize(
+        onStatus:(val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val')
+      );
+      if(available){
+        setState(() {
+          isListening=true;
+        });
+        speech.listen(
+          onResult: (val) => setState(()=>{
+            _searchText = val.recognizedWords,
+            _controller.text = val.recognizedWords,
+            _controller.selection = TextSelection.fromPosition(
+              TextPosition(offset: _controller.text.length),
+            ),
+          })
+        );
+      }
+    }else{
+      setState(() {
+        isListening = false;
+      });
+      speech.stop();
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,12 +66,21 @@ class _SearchScreenState extends State<SearchScreen> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: _controller,//luu tu sau khi noi tu mic 
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
                   prefixIcon: const Icon(Icons.search),
                   hintText: 'Ví dụ: Burger rau củ',
-                  suffixIcon: const Icon(Icons.tune),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.mic),
+                      onPressed: () {
+                        // gọi chức năng ghi âm
+                        
+                        onlisten();
+                        
+                      }
+                  ),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(16),
                   ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import flutter_inappwebview_macos
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
